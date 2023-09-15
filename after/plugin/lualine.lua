1f436e032e7a351d4fc9b14bf46541ff9bfffc64
Josef Podany <podany.pepa@gmail.com>
2023-09-15 14:13:27 +0200

    fix file name in liuline (status line): relative path

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -10,7 +10,7 @@ require("lualine").setup({
 		},
 		ignore_focus = {},
 		always_divide_middle = true,
-		globalstatus = false,
+		globalstatus = true,
 		refresh = {
 			statusline = 1000,
 			tabline = 1000,
@@ -20,8 +20,12 @@ require("lualine").setup({
 	sections = {
 		lualine_a = { "mode" },
 		lualine_b = { "branch", "diff", "diagnostics" },
-		lualine_c = { "filename" },
-		lualine_x = { "encoding", "fileformat", "filetype" },
+		lualine_c = { {
+			"filename",
+			file_status = true,
+			path = 1,
+		} },
+		lualine_x = { "filetype" },
 		lualine_y = { "progress" },
 		lualine_z = { "location" },
 	},
