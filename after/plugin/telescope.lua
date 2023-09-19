89694604415c362df6bbb6b80458bbbb9133f2a4
Josef Podany <podany.pepa@gmail.com>
2023-09-19 13:12:59 +0200

    telescope remaps

---
+++ after/plugin/telescope.lua
@@ -0,0 +1,14 @@
+local builtin = require("telescope.builtin")
+
+vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+vim.keymap.set("n", "<leader>gf", builtin.git_files, {})
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+vim.keymap.set("n", "<leader>ts", function()
+	builtin.grep_string({ search = vim.fn.input("Grep > ") })
+end)
+
+vim.cmd([[
+nnoremap <leader>fw :execute 'Telescope live_grep default_text=' . expand('<cword>')<cr>
+]])
