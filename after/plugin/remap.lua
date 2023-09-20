00c597b2c7ee0cfc21c5bb82c01f43be5f166d02
Josef Podany <podany.pepa@gmail.com>
2023-09-20 13:39:18 +0200

    move telescope keympas to remap lua file

--- after/plugin/remap.lua
+++ after/plugin/remap.lua
@@ -35,3 +35,37 @@ vim.keymap.set("n", "<leader>Y", '"+Y')
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>td", ":Trouble workspace_diagnostics<CR>")
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+
+local builtin = require("telescope.builtin")
+
+vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+vim.keymap.set("n", "<leader>gf", builtin.git_files, {})
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+vim.keymap.set("n", "<leader>ts", function()
+	builtin.grep_string({ search = vim.fn.input("Grep > ") })
+end)
+vim.keymap.set("n", "<Leader><Leader>", function()
+	builtin.find_files({
+		layout_strategy = "vertical",
+		layout_config = { width = 0.8 },
+	})
+end)
+
+vim.keymap.set("n", "<Leader>tR", function()
+	builtin.lsp_references({
+		layout_strategy = "vertical",
+	})
+end)
+
+vim.keymap.set("n", "<Leader>tg", function()
+	builtin.grep_string({
+		layout_strategy = "vertical",
+		layout_config = { width = 0.8 },
+	})
+end)
+
+vim.cmd([[
+nnoremap <leader>fw :execute 'Telescope live_grep  default_text=' . expand('<cword>')<cr>
+]])
--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -8,37 +8,3 @@ require("telescope").setup({
 		},
 	},
 })
-
-local builtin = require("telescope.builtin")
-
-vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-vim.keymap.set("n", "<leader>gf", builtin.git_files, {})
-vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
-vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
-vim.keymap.set("n", "<leader>ts", function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") })
-end)
-vim.keymap.set("n", "<Leader><Leader>", function()
-	builtin.find_files({
-		layout_strategy = "vertical",
-		layout_config = { width = 0.8 },
-	})
-end)
-
-vim.keymap.set("n", "<Leader>tR", function()
-	builtin.lsp_references({
-		layout_strategy = "vertical",
-	})
-end)
-
-vim.keymap.set("n", "<Leader>tg", function()
-	builtin.grep_string({
-		layout_strategy = "vertical",
-		layout_config = { width = 0.8 },
-	})
-end)
-
-vim.cmd([[
-nnoremap <leader>fw :execute 'Telescope live_grep  default_text=' . expand('<cword>')<cr>
-]])
