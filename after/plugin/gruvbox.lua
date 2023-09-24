3ac6298057e4b4334f22ccd66580082c12cb72fa
Josef Podany <podany.pepa@gmail.com>
2023-09-24 12:19:52 +0200

    cleaning gruvbox color config

--- after/plugin/gruvbox.lua
+++ after/plugin/gruvbox.lua
@@ -27,19 +27,20 @@ vim.cmd("colorscheme gruvbox")
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
-vim.cmd("highlight clear SignColumn")
-vim.cmd("hi CursorLineNr guibg=none ")
-vim.cmd("hi DiagnosticSignWarn ctermfg=yellow ctermbg=none")
-vim.cmd("hi DiagnosticSignError guifg=red ctermbg=none")
-vim.cmd("hi DiagnosticSignInfo guifg=Blue ctermbg=none")
-vim.cmd("hi DiagnosticSignHint guifg=Green ctermbg=none")
-vim.cmd("hi GitSignsAdd guifg=Green3 guibg=none")
-vim.cmd("hi GitSignsChange guifg=Yellow2 guibg=none")
-vim.cmd("hi GitSignsDelete guifg=OrangeRed1 guibg=none")
-vim.cmd("hi Pmenu guibg=NONE ctermbg=none")
-vim.cmd("hi WinSeparator guibg=NONE")
 
 vim.cmd([[
+highlight clear SignColumn
+hi CursorLineNr guibg=none
+hi DiagnosticSignWarn ctermfg=yellow ctermbg=none
+hi DiagnosticSignError guifg=red ctermbg=none
+hi DiagnosticSignInfo guifg=Blue ctermbg=none
+hi DiagnosticSignHint guifg=Green ctermbg=none
+hi GitSignsAdd guifg=Green3 guibg=none
+hi GitSignsChange guifg=Yellow2 guibg=none
+hi GitSignsDelete guifg=OrangeRed1 guibg=none
+hi Pmenu guibg=NONE ctermbg=none
+hi WinSeparator guibg=NONE
+
 let &t_SI = "\e[6 q"
 let &t_EI = "\e[2 q"
 let &t_ZH="\e[3m"
