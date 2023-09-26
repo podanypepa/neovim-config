31903f01fb7e8e6672c25977e1aac458d6b4d455
Josef Podany <podany.pepa@gmail.com>
2023-09-26 10:58:09 +0200

    formatter config for js, ts and html files

--- after/plugin/formatter.lua
+++ after/plugin/formatter.lua
@@ -16,6 +16,15 @@ require("formatter").setup({
 		json = {
 			require("formatter.filetypes.json").jq,
 		},
+		javascript = {
+			require("formatter.filetypes.javascript").prettier,
+		},
+		typescript = {
+			require("formatter.filetypes.typescript").prettier,
+		},
+		html = {
+			require("formatter.filetypes.html").prettier,
+		},
 		lua = {
 			require("formatter.filetypes.lua").stylua,
 
