66704edc28210bfaf41352bd150dfdd0714eda60
Josef Podany <podany.pepa@gmail.com>
2024-02-22 09:41:01 +0100

    update confog: add neo-tree

--- init.lua
+++ init.lua
@@ -122,14 +122,16 @@ require("lazy").setup({
 		},
 	},
 	-- { "rafamadriz/friendly-snippets" },
-	-- {
-	-- 	"nvim-neo-tree/neo-tree.nvim",
-	-- 	branch = "v3.x",
-	-- 	dependencies = {
-	-- 		"nvim-lua/plenary.nvim",
-	-- 		"MunifTanjim/nui.nvim",
-	-- 	},
-	-- },
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+			"MunifTanjim/nui.nvim",
+			"3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+		},
+	},
 	{
 		"folke/todo-comments.nvim",
 		dependencies = { "nvim-lua/plenary.nvim" },
