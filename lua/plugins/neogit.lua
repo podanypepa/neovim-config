cb2f31cd3af16492a3a564ad1d47c5a9dd46aa16
Josef Podany <podany.pepa@gmail.com>
2024-09-05 11:11:11 +0200

    update repo

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -13,13 +13,23 @@ return {
 		require("telescope").setup({
 			defaults = {
 				layout_strategy = "vertical",
-			}
+			},
 		})
 		require("neogit").setup({
 			commit_view = {
 				kind = "split",
 				verify_commit = vim.fn.executable("gpg") == 1, -- Can be set to true or false, otherwise we try to find the binary
 			},
+			highlight = {
+				italic = true,
+				bold = true,
+				underline = true,
+			},
+			auto_refresh = true,
+			sort_branches = "-committerdate",
+			disable_line_numbers = true,
+			auto_show_console = true,
+			auto_close_console = true,
 		})
 	end,
 }
