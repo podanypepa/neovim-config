088c3699bbb43b59b8e2b7e405d662cb2d9efaa2
Josef Podany <podany.pepa@gmail.com>
2025-06-16 18:54:06 +0200

    new: update fzf config

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -2,6 +2,8 @@ return {
 	"ibhagwan/fzf-lua",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	opts = {
+		file_icon_padding = " ",
+		fzf_bin = "sk",
 		winopts = {
 			preview = {
 				layout = "vertical",
