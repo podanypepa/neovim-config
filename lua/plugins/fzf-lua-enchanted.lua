d59d10b98e0e72a8107087be7c7e1914367fd2ad
Josef Podany <podany.pepa@gmail.com>
2025-07-12 09:24:05 +0200

    remove fzf-lua-enchanted plugin

--- lua/plugins/fzf-lua-enchanted.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"otavioschwanck/fzf-lua-enchanted-files",
-	dependencies = { "ibhagwan/fzf-lua" },
-	config = function()
-		vim.g.fzf_lua_enchanted_files = {
-			max_history_per_cwd = 50,
-		}
-	end,
-}
