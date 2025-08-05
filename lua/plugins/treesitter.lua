a88c26351006ba0a1eaacc563e2f253176c39b72
Josef Podany <podany.pepa@gmail.com>
2025-08-05 11:31:48 +0200

    update treesitter config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -40,8 +40,8 @@ return {
 
 			highlight = {
 				enable = true,
-				disable = function(lang, buf)
-					local max_filesize = 100 * 1024 -- 100 KB
+				disable = function(_, buf)
+					local max_filesize = 100 * 1024
 					local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
 					if ok and stats and stats.size > max_filesize then
 						return true
