529f396ca87221de727f2046ea7ebbb34bca618a
Josef Podany <podany.pepa@gmail.com>
2025-08-08 09:35:37 +0200

    new autocmd for terminal

--- init.lua
+++ init.lua
@@ -4,3 +4,12 @@ require("config.colors")
 require("config.lsp")
 require("config.keymap")
 require("config.autocmd")
+
+vim.api.nvim_create_autocmd("TermOpen", {
+	group = vim.api.nvim_create_augroup("custom-term-open", { clear = true }),
+	callback = function()
+		vim.opt.number = false
+		vim.opt.relativenumber = false
+		vim.cmd("startinsert")
+	end,
+})
